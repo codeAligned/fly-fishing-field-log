ab3cd9c0c866a0e5c5aa75a080f4df74280bf2d1
gyeon <gyeon19@gmail.com>
2016-11-17 15:37:00 -0500

    reports method added on river

--- app/models/river.rb
+++ app/models/river.rb
@@ -4,6 +4,21 @@ class River < ApplicationRecord
   has_many :river_flies
   has_many :flies, through: :river_flies
 
+  def reports(river)
+    name = river.upcase
+    river_name = River.find_by(name: name)
+    reports = Report.where(river_id: river_name.id)
+  end
+
+
+  def biggest_fish(river)
+    # ie) blue
+    # name = blue
+    # find all reports that have the name blue as their river. find by the river_id
+
+    river_name = River.find_by(name: river)
+    reports = Report.where(river_id: river_name.id)
+  end
 
 end
 
@@ -17,4 +32,4 @@ end
     # ie) weather, water, fly
 # find most common fly by river
 # return all the river's stations
-# 
+#
