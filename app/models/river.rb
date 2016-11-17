217c0e013c109557a116674bfac9ab0480b7b929
gyeon <gyeon19@gmail.com>
2016-11-17 00:35:36 -0500

    styling is in, nav bar needs fixing

--- app/models/river.rb
+++ app/models/river.rb
@@ -3,4 +3,18 @@ class River < ApplicationRecord
 
   has_many :river_flies
   has_many :flies, through: :river_flies
+
+
 end
+
+
+
+### want to have methods that:
+
+#find the biggest fish by river
+  # then be able to see the conditions that
+  # allowed them to catch that:
+    # ie) weather, water, fly
+# find most common fly by river
+# return all the river's stations
+# 
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,16 @@
 class Station < ApplicationRecord
   belongs_to :river
+  # 
+  # def self.popular(name)
+  #   # first find all the stations with that river's id
+  #   river_id = River.where(name: name).id
+  #   binding.pry
+  #   stations = Station.find_by(river_id: river_id)
+  #   stations.each do |station|
+  #     station.station_name
+  #   end
+  #
+  # end
+
+
 end
