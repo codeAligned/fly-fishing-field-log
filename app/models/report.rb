6fcefa321c0f67fc44c2626449b68b4568ea83b0
gyeon <gyeon19@gmail.com>
2016-11-22 17:19:05 -0500

    number of fish caught method added on report

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,5 +7,11 @@ class Report < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :fish, :weather, :fly, :river, :water
 
-  
+  def num_fish_caught(river)
+    river = river.upcase
+    river_id = River.find_by(name: river).id
+    num_fish_caught = self.where(river_id: river_id).count
+  end
+
+
 end
--- app/models/river.rb
+++ app/models/river.rb
@@ -4,16 +4,12 @@ class River < ApplicationRecord
   has_many :river_flies
   has_many :flies, through: :river_flies
 
-  def reports
-    reports = Report.where(river_id: self.id)
-  end
-
 
   def biggest_fish
     # ie) blue
     # name = blue
     # find all reports that have the name blue as their river. find by the river_id
-    reports.map do |report|
+    self.reports.map do |report|
       # returns all the fish
       fish_info = Fish.where(id: report.fish_id)
       fish = fish_info.order(length: :asc)
@@ -21,17 +17,9 @@ class River < ApplicationRecord
     biggest_fish = fish.first
   end
 
-  def stations
-    stations = Station.where(river_id: self.id)
-  end
+  #have biggest 5 fish method?
+  #Rivers with the most reports
+
 
 
 end
-
-### want to have methods that:
-
-#find the biggest fish by river
-  # then be able to see the conditions that
-  # allowed them to catch that:
-    # ie) weather, water, fly
-# find most common fly by river
